c1d9f6e11eb95ed7f75f4e2c60b2ea761fbef6de
Kuarma <mas.schibli@kuarma.com>
2026-03-31 22:14:27 +0200

    Treesitter keymaps

--- lua/kuarma/plugins/treesitter.lua
+++ lua/kuarma/plugins/treesitter.lua
@@ -2,16 +2,20 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				opts = {
+					select = {
+						lookahead = true,
+						include_surrounding_whitespace = false,
+					},
+				},
+			},
 		},
 		lazy = false,
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter").setup({
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				-- Rust: cargo install --locked tree-sitter-cli
-				-- https://github.com/tree-sitter/tree-sitter/blob/master/crates/cli/README.md
 				auto_install = true,
 				highlight = {
 					enable = true,
@@ -80,11 +84,50 @@ return {
 					vim.opt.foldlevel = 10
 				end,
 			})
+			vim.keymap.set({ "n", "x", "o" }, "]m", function()
+				require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]]", function()
+				require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]o", function()
+				require("nvim-treesitter-textobjects.move").goto_next_start(
+					{ "@loop.inner", "@loop.outer" },
+					"textobjects"
+				)
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]s", function()
+				require("nvim-treesitter-textobjects.move").goto_next_start("@local.scope", "locals")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "]z", function()
+				require("nvim-treesitter-textobjects.move").goto_next_start("@fold", "folds")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "]M", function()
+				require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "][", function()
+				require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "[m", function()
+				require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[[", function()
+				require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "[M", function()
+				require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[]", function()
+				require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+			end)
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
-		dependencies = "nvim-treesitter/nvim-treesitter",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
 			require("treesitter-context").setup({
 				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
