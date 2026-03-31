ec1433fc52188e6d4c2de7d0b1d30ec2221ed951
Kuarma <mas.schibli@kuarma.com>
2026-03-31 21:45:38 +0200

    Trouble configured

--- lua/kuarma/plugins/lsp.lua
+++ lua/kuarma/plugins/lsp.lua
@@ -14,13 +14,24 @@ return {
 					},
 				},
 			},
+			{
+				"folke/trouble.nvim",
+				opts = {},
+				cmd = "Trouble",
+				keys = {
+					{
+						"<leader>xx",
+						"<cmd>Trouble diagnostics toggle<cr>",
+						desc = "Diagnostics (Trouble)",
+					},
+				},
+			},
 			"stevearc/conform.nvim",
 			"mason-org/mason-lspconfig.nvim",
 			"mason-org/mason.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			"saghen/blink.cmp",
 			"lewis6991/gitsigns.nvim",
-			"folke/trouble.nvim",
 		},
 		config = function()
 			-- Lsp / Mason setup
